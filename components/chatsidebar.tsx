a7c6f71811ec5b4a76240519c16da318673d0a97
Giang <463189@student.fontys.nl>
2021-10-20 11:31:58 +0200

    change and refactor code in chatsidebar.tsx

--- components/chatsidebar.tsx
+++ components/chatsidebar.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Conversations from "./conversations";
 
 interface Props {
-    title: string,
-    icon: string,
 
 }
 
@@ -12,17 +10,20 @@ function Chatsidebar(props: Props) {
         <>
             <div style={{ width: '300px' }} className={"flex-col"}>
                 <div className={"flex h-8"}>
-                    <div className={"flex-col px-6 mx-2"}>
-                        <h3>{props.title ? props.title : "Chats"}</h3>
+                    <div className={" mx-2"}>
+                        <h3>Chats</h3>
                     </div>
-                    <div className={"flex flex-row-reverse px-6 mx-2 w-full items-center"}>
-                        <i className={props.icon ? props.icon : "fa fa-arrow-circle-left h-8 w-8"}></i>
-                    </div>
+
                 </div>
-                <div className={"flex flex-row border-b-2"}>
-                    <input className={"rounded-full py-2 px-6 m-2 w-full"} type="search" name="search" placeholder="Search" />
+                <div className={"flex flex-row border-b-2 p-2"}>
+                    <span className={"input-left icon inline-flex items-center p-2"}>
+                        <span aria-hidden="true" className="cm-icon cm-icon-search"></span>
+                    </span>
+                    <input style={{paddingLeft: '15px'}} className={"border rounded-full w-full"} type="search" name="search" placeholder="Search" />
                     <button type="submit"></button>
+
                 </div>
+
                 <div style={{ height: '100vh' }} className={"overflow-auto border-r-2"}>
                     <Conversations channel={"/twitter.svg"} name={"Giang"} content={"I need help!!!!"} time={"13 oct. 2020"}/>
                 </div>
