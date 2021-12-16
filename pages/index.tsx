215af8b8b7f923468f1400bfe6d29ebf9f58b1aa
Amy <dragonhel5@gmail.com>
2021-12-16 12:27:54 +0100

    remove log

--- pages/index.tsx
+++ pages/index.tsx
@@ -138,7 +138,6 @@ export default function Home() {
     const [showProfanity, setShowProfanity] = useState(false);
 
     function handleProfanityClick() {
-        console.log(showProfanity)
         setShowProfanity(!showProfanity)
     }
 
