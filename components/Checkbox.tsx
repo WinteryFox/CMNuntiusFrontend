80f97b1feb51617abfd696b1bd028ef32f396daa
AmyFoxie <dragonhel5@gmail.com>
2021-12-21 18:08:40 +0100

    fix translation; make cursor pointer on toggle hover

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -7,10 +7,10 @@ export default function Checkbox(props: {
     children?: ReactNode
 }) {
     return (
-        <label className={"flex items-center select-none justify-between"}>
+        <label className={"flex items-center select-none justify-between cursor-pointer"}>
             {props.children}
             <input type={"checkbox"} checked={props.checked} onChange={props.onChange} disabled={props.disabled}
-                   className={"inline-block mr-2 dark:bg-black appearance-none text-blue-500 w-4 h-4 border rounded-3xl checked:bg-blue-500"}/>
+                   className={"cursor-pointer inline-block mr-2 dark:bg-black appearance-none text-blue-500 w-4 h-4 border rounded-3xl checked:bg-blue-500"}/>
         </label>
     )
 }
